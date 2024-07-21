bea30992a7e401bfb0928bcc2f7c68bf29b7d5e2
circumspectjunior <giftojo200047@yahoo.com>
2024-07-21 17:06:16 +0200

    17_05_july_21

---
+++ loops.js
@@ -0,0 +1,23 @@
+
+let num = 0;
+
+/*
+for(let i = 0; i <= 10; i++){
+
+    console.log(i);
+
+} */
+
+    let i = 0;
+
+    while (i < 10){
+        i++
+        console.log(i);
+
+    }
+
+
+
+
+
+
